ade8e4fe66221b3edb8b857ea3725ed85dab5aee
Travis Baker <travi5jab@traviss-mbp.attlocal.net>
2019-08-14 22:59:53 -0700

    extra file removal

--- HTML_script/Educational_HTML/Form/form.js
+++
@@ -1,4 +0,0 @@
-function greeter(){
-  let userName = document.getElementById("userInput").value
-  document.getElementById("reply").innerHTML = `Welcome, ${userName}`
-}
--- Javascript_scripts/JavaScript1.js
+++
@@ -1,1 +0,0 @@
-script.log("Hello")
